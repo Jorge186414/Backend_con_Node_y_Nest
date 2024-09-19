b322a1c061264e6256175a45e75496dba26e141c
Jorge186414 <jorgaismiranda@gmail.com>
2024-09-18 23:46:38 -0600

    Add: Clase de PokeApi con fetch para el principio de sustitucion, y formateo de codigo

--- 01-TypeScript/src/api/pokeApi.adapter.ts
+++ 01-TypeScript/src/api/pokeApi.adapter.ts
@@ -1,26 +1,36 @@
 import axios from 'axios'
 
+export class PokeAPIFetchAdapter {
+
+   async get<T>(url: string): Promise<T> {
+      const response = await fetch(url)
+      const data: T = await response.json()
+
+      return data
+   }
+}
+
 export class PokeAPIAdapter {
 
-  private readonly axios = axios
+   private readonly axios = axios
 
-  async get(url: string) {
-    // Peticion HTTP GET
-    const {data} = await this.axios.get(url)
-    return data
-  }
+   async get<T>(url: string): Promise<T> {
+      // Peticion HTTP GET
+      const { data } = await this.axios.get<T>(url)
+      return data
+   }
 
-  async post(url: string, data: any) {
+   async post(url: string, data: any) {
 
-  }
+   }
 
-  async patch(url: string, data: any) {
+   async patch(url: string, data: any) {
 
-  }
+   }
 
-  async delet(url: string, data: any) {
+   async delet(url: string, data: any) {
 
-  }
+   }
 }
 
 
