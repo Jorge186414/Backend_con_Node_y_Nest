0f98380b47cb5b6c72abb2388a25492367e31745
Jorge186414 <jorgaismiranda@gmail.com>
2024-09-18 23:47:33 -0600

    Add: Importacion de la nueva clase, creacion de instancia de esta y formateo de codigo

--- 01-TypeScript/src/bases/04-injections.ts
+++ 01-TypeScript/src/bases/04-injections.ts
@@ -1,42 +1,43 @@
-import { Move } from '../interfaces/pokeapi-response.interface';
-import { PokeAPIAdapter } from '../api/pokeApi.adapter';
+import { Move, PokeAPIResponse } from '../interfaces/pokeapi-response.interface';
+import { PokeAPIAdapter, PokeAPIFetchAdapter } from '../api/pokeApi.adapter';
 
 export class Pokemon {
 
-  get imageUrl(): string {
-    return `https://pokemon.com/${this.id}.jpg`;
-  }
+	get imageUrl(): string {
+		return `https://pokemon.com/${this.id}.jpg`;
+	}
 
-  constructor(
-    public readonly id: number,
-    public name: string,
-    // Todo: inyectar dependencias
-    private readonly http: PokeAPIAdapter
+	constructor(
+		public readonly id: number,
+		public name: string,
+		// Todo: inyectar dependencias
+		private readonly http: PokeAPIAdapter
 
-  ) { }
+	) { }
 
-  scream() {
-    console.log(`${this.name.toUpperCase()}!!!`);
-  }
+	scream() {
+		console.log(`${this.name.toUpperCase()}!!!`);
+	}
 
-  speak() {
-    console.log(`${this.name}, ${this.name}`);
-  }
+	speak() {
+		console.log(`${this.name}, ${this.name}`);
+	}
 
-  async getMoves(): Promise<Move[]> {
-    // const { data } = await axios.get<PokeAPIResponse >('https://pokeapi.co/api/v2/pokemon/4'); cambiamos esto por
-    // Se hace la inyeccion de dependencias para mejor correccion de errores en caso de una actualizacion en un futuro
-    // dato que no tendriamos que cambiar la primer linea que se comento, solo nos dirigimos a nuestro adaptador y lo c
-    // cambiamos directament ahi para poder seguir usandolo como dependencia inyectada
-    const data = await this.http.get('https://pokeapi.co/api/v2/pokemon/4')
-    console.log(data.moves);
+	async getMoves(): Promise<Move[]> {
+		// const { data } = await axios.get<PokeAPIResponse >('https://pokeapi.co/api/v2/pokemon/4'); cambiamos esto por
+		// Se hace la inyeccion de dependencias para mejor correccion de errores en caso de una actualizacion en un futuro
+		// dato que no tendriamos que cambiar la primer linea que se comento, solo nos dirigimos a nuestro adaptador y lo c
+		// cambiamos directament ahi para poder seguir usandolo como dependencia inyectada
+		const data = await this.http.get<PokeAPIResponse>('https://pokeapi.co/api/v2/pokemon/4')
+		console.log(data.moves[0]);
 
-    return data.moves;
-  }
+		return data.moves;
+	}
 
 }
 
 const pokeApi = new PokeAPIAdapter()
+const pokeApiFetch = new PokeAPIFetchAdapter()
 
 export const charmander = new Pokemon(4, 'Charmander', pokeApi);
 
