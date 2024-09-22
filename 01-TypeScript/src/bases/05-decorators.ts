158027eb7ded11723b80481339f3085c99a9346e
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-22 00:01:25 -0600

    Add: Decorador para sobreescribir una clase ya definida

--- 01-TypeScript/src/bases/05-decorators.ts
+++ 01-TypeScript/src/bases/05-decorators.ts
@@ -1,7 +1,29 @@
+// El decorador tiene acceso al definicion de la clase, y podra 
+// expandir, extender agregar, bloquear o eliminar funcionalidades
+// e inclusive sobreescribir toda la clase o crear una nueva basada 
+// en esta, no son mas que una simple funcion, pero dependiendo de 
+// como se utilice puede hacer todo lo previamente mencionado
+
 const MyDecorator = () => {
     // Llevaria referencia al target
     return (target: Function) => {
-        console.log(target)
+        return NewPokemon
+    }
+}
+
+class NewPokemon {
+    constructor(
+        public readonly id: number,
+        public name: string
+    ) {
+    }
+
+    scream() {
+        console.log(`No quero !!!`)
+    }
+
+    speak() {
+        console.log(`No quiere hablar el pokemon!`)
     }
 }
 
