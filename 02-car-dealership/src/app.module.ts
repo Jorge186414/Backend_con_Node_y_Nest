88b57f8e81e84eff9c5ed96392c44e8d0e084711
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-23 00:21:26 -0600

    Fix: Se elimino la carpteta como submodulo y se agrego como una carpta normal al repo local

---
+++ 02-car-dealership/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+
+// building block de Nest, agrupan o deagrupan funcionalidades especificas por dominio
+// Modulo root o principal, tiene referencias a toda la aplicacion
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class AppModule { }
