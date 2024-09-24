9b2370f94c6823e141071d7910f663bb334ac060
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-23 21:37:03 -0600

    Add: Metodo para obtener un carro por ID

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
-   
+
+   // Se separan los carros para poder recuperarlos en base a su ID
+   private cars = ['Toyota', 'Subaru', 'Nissan', 'Chevrolet', 'Ford']
+
    @Get()
-   getAllCars(){
-      return ['Toyota', 'Subaru', 'Nissan', 'Chevrolet', 'Ford']
+   getAllCars() {
+      return this.cars
+   }
+
+   // Metodo para recuperar un carro por su id
+   @Get(':id')
+   // Tratamos al id como string, posteriormente se va a cambiar por number
+   getCarByID(@Param('id') id: string) {
+      // retornarmos el nombre del carro en base a su posicion en el arrgelo
+      return this.cars[id]
    }
 }
