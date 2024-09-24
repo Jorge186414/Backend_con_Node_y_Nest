6b4ecb5a8554cef2b79877ca27835ead1847e311
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-23 22:16:50 -0600

    Add: Inyecction de cars.service.ts como dependencia

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,19 +1,25 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-   
+   constructor(
+      // Inyectamos como dependencia nuestro servicio(provider)
+      private readonly carsService: CarsService
+   ) { }
+
+   // Metodo para obtener todos los carros
    @Get()
    getAllCars() {
-      return this.cars
+      return this.carsService.findAll()
    }
 
    // Metodo para recuperar un carro por su id
    @Get(':id')
    // Tratamos al id como string, posteriormente se va a cambiar por number
    getCarByID(@Param('id') id: string) {
-      // retornarmos el nombre del carro en base a su posicion en el arrgelo
-      return this.cars[id]
+      // retornarmos el nombre del carro en base a su posicion en el arreglo
+      return this.carsService.findOneByID(+id)
    }
 }
