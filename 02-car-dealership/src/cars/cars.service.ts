65e1e7a11befbf5c988209cbd7134b92456cea1c
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-23 21:53:50 -0600

    Add: Arreglo de carros en forma de objetos para su futura recuperacion

--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -3,4 +3,32 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class CarsService {
    
+   private cars = [
+      {
+         id: 1,
+         brand: 'Toyota',
+         model: 'Corolla'
+      },
+      {
+         id: 2,
+         brand: 'Honda',
+         model: 'Civic'
+      },
+      {
+         id: 3,
+         brand: 'Ford',
+         model: 'Mustang'
+      },
+      {
+         id: 4,
+         brand: 'Chevrolet',
+         model: 'Camaro'
+      },
+      {
+         id: 5,
+         brand: 'Tesla',
+         model: 'Model 3'
+      }
+   ]
+
 }
