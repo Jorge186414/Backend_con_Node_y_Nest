2ae2c6f88c61cffed899c7810c7966f59c3daf15
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-25 00:20:39 -0600

    Metodo para la actualizacion de un carro, este metodo se podra usar despues de inyectar la clase donde esta como dependecia

--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -1,8 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './interfaces/cars.interface';
 import { v4 as uuid } from 'uuid'
-import { CreateCarDto } from './dto/create-car.dto';
-
+import { CreateCarDto, UpdateCarDto } from './dto';
 
 @Injectable()
 export class CarsService {
@@ -77,6 +76,21 @@ export class CarsService {
 
       this.cars.push(car)
       return car
+   }
+
+   update(id: string, updateCarDto: UpdateCarDto) {
+
+      let carDB = this.findOneByID(id)
+
+      this.cars = this.cars.map(car => {
+         if (car.id === id) {
+            carDB = { ...carDB, ...updateCarDto, id }
+            return carDB
+         }
+         return car
+      })
+
+      return carDB
    }
 
 }
