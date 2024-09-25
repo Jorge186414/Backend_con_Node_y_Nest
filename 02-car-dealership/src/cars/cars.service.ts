b72f222c0bb509ecda732be6827eeb521bb68ce3
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-24 21:13:36 -0600

    Modified: Generacion del ID de nuestro carro con UUID, cambio del ID como parametro de Number a String

--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -1,31 +1,34 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid'
+
 
 @Injectable()
 export class CarsService {
 
-   private cars = [
+   private cars: Car[] = [
       {
-         id: 1,
+         id: uuid(),
          brand: 'Toyota',
          model: 'Corolla'
       },
       {
-         id: 2,
+         id: uuid(),
          brand: 'Honda',
          model: 'Civic'
       },
       {
-         id: 3,
+         id: uuid(),
          brand: 'Ford',
          model: 'Mustang'
       },
       {
-         id: 4,
+         id: uuid(),
          brand: 'Chevrolet',
          model: 'Camaro'
       },
       {
-         id: 5,
+         id: uuid(),
          brand: 'Tesla',
          model: 'Model 3'
       }
@@ -37,12 +40,12 @@ export class CarsService {
    }
 
    // Metodo para recuperar los datos de un carro en base a un id
-   findOneByID(id: number) {
+   findOneByID(id: string) {
       const car = this.cars.find(car => car.id === id)
 
       // Validacion de la existencia del carro
       if (!car) throw new NotFoundException(`Car with id ${id} not found`)
-         
+
       return car
    }
 }
