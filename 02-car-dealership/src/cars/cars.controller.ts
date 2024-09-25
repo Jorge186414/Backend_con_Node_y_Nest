64354c0b111e6494980d95786568582a686fef61
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-24 21:15:54 -0600

    Modified: Cambio en la forma en que mandamos los parametros

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { Car } from './interfaces/car.interface';
 
 @Controller('cars')
 export class CarsController {
@@ -18,25 +19,28 @@ export class CarsController {
    // Metodo para recuperar un carro por su id
    @Get(':id')
    // Tratamos al id como string, posteriormente se va a cambiar por number
-   getCarByID(@Param('id', ParseIntPipe) id: number) {
+   getCarByID(@Param('id') id: string) {
       // retornarmos el nombre del carro en base a su posicion en el arreglo
       return this.carsService.findOneByID(id)
    }
 
+   // Metodo para obtener crear un objeto
    @Post()
    createCar(@Body() body: any) {
       return body
    }
 
+   // Metodo para editar/actualizar un objeto
    @Patch(':id')
    updateCar(
-      @Param('id', ParseIntPipe) id: number,
-      @Body() body: any) {
+      @Param('id') id: string,
+      @Body() body: Car) {
       return body
    }
 
+   // Metodo para eliminar un objeto
    @Delete(':id')
-   deleteCar(@Param('id', ParseIntPipe) id: number) {
+   deleteCar(@Param('id') id: string) {
       return {
          method: 'DELETE',
          id: id
