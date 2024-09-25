8868924199be9f5973ac41f39cc506070deeb7d7
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-24 22:35:09 -0600

    Add: Especificacion de que tipo de datos sera el valor del objeto

--- 02-car-dealership/src/cars/dto/create-car.dto.ts
+++ 02-car-dealership/src/cars/dto/create-car.dto.ts
@@ -1,5 +1,10 @@
+import { IsString } from "class-validator"
+
 export class CreateCarDto {
+
+   @IsString({ message: `Brand must be a string ` })
    readonly brand: string
 
+   @IsString()
    readonly model: string
 }
