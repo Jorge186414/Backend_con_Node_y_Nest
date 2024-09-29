078416f5278ed7c90602574e87c942c3fe0451b7
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-28 22:36:07 -0600

    Add: Uso de Nest CLI para la creacion de un recurso

--- 02-car-dealership/src/app.module.ts
+++ 02-car-dealership/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
+import { BrandsModule } from './brands/brands.module';
 
 // building block de Nest, agrupan o deagrupan funcionalidades especificas por dominio
 // Modulo root o principal, tiene referencias a toda la aplicacion
 @Module({
-  imports: [CarsModule],
+  imports: [CarsModule, BrandsModule],
   controllers: [],
   providers: [],
   exports: [],
