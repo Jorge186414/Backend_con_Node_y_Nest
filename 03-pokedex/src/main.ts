a6717a2c14bce3cb09f45055630b12b2d8ed01c5
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-10-30 23:25:52 -0600

    Add: Validacion global de campos

--- 03-pokedex/src/main.ts
+++ 03-pokedex/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api/v2')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Aunque se manden mas datos que no estan en el DTO
+      // los va a eliminar de forma automatica y solo mostrara los definidos en el DTO
+      forbidNonWhitelisted: true // Aunque whitelist los elimina, este se encarga de 
+      // decir que los datos extras que se estan mandando no deberian de ir
+    })
+  )
+
   await app.listen(3000);
+
 }
 bootstrap();
