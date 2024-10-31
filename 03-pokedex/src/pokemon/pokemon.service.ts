b9bc248ce5bcb3f3a009b03d8274bff3646b962d
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-10-31 00:45:53 -0600

    Add: Actualizacion de un pokemon y cambio en la forma en como se manejaba el error 11000

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -21,11 +21,7 @@ export class PokemonService {
       const pokemon = await this.pokemonModel.create(createPokemonDto)
       return pokemon
     } catch (error) {
-      if (error.code === 11000) {
-        throw new BadRequestException(`Pokemon exists in db ${JSON.stringify(error.keyValue)}`)
-      }
-      console.log(error)
-      throw new InternalServerErrorException(`Can't create Pokemin -Check server logs`)
+      this.handleException(error)
     }
 
 
@@ -60,11 +56,33 @@ export class PokemonService {
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+
+    const pokemon = await this.findOne(term)
+    if (updatePokemonDto.name)
+      updatePokemonDto.name = updatePokemonDto.name.toLocaleLowerCase()
+
+    try {
+      await pokemon.updateOne(updatePokemonDto, { new: true })
+      return { ...pokemon.toJSON(), ...updatePokemonDto }
+
+    } catch (error) {
+      this.handleException(error)
+    }
+
+
   }
 
   remove(id: number) {
     return `This action removes a #${id} pokemon`;
   }
+
+  private handleException(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(`Pokemon exists in db ${JSON.stringify(error.keyValue)}`)
+    }
+    console.log(error)
+    throw new InternalServerErrorException(`Can't create Pokemin -Check server logs`)
+  }
+
 }
