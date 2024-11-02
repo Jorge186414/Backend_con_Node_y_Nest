1d9ae3b323b576d81b8b016599c90aa0a26c3332
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-11-02 00:42:58 -0600

    Add: Importacion del modulo previamente exportado e insercion de los datos a la BD en Mongo

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import axios, { AxiosInstance } from 'axios';
 
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 
 @Injectable()
@@ -9,22 +12,28 @@ export class SeedService {
 
   private readonly axios: AxiosInstance = axios
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) { }
+
   async executeSeed() {
 
     // Una vez que tenemos la interfaz creada le asignamos ese tipo a nuestra respuesta
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
 
     // Extraemos el nombre del pokemon y el id de la respuesta a la API
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       // El nombre si viene en la respuesta de la API pero pero el id viene en la url
       //? Separamos la la url de la respuesta cada que haya una diagonal(/)
       const segments = url.split('/')
       //? Una vez separada la url obtenemos el id en base a la posicion en que separo, en este caso la penultima 
       const no = +segments[segments.length - 2]
-      //? Mostramos el name y el id del pokemon
-      console.log({ name, no })
+      //? Insertamos lo registros en la base de datos
+      const pokemon = await this.pokemonModel.create({ name, no })
+
     })
 
-    return data.results;
+    return 'Seed Excecuted';
   }
 }
