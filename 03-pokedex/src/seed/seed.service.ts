f3c6770f66a4d2d2db313ddb520f6736ee8d43ba
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-11-03 04:05:22 -0600

    Add: Forma 1 de hacer multiples inserciones a la base de datos

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -19,8 +19,14 @@ export class SeedService {
 
   async executeSeed() {
 
+    //! Dado que va a a ser una Seed que inserte datos a la BD, antes de todo eliminamos los datos existentes en la BD
+    await this.pokemonModel.deleteMany()  //! Esto equivale a Delete * FROM nombre_tabla
+
     // Una vez que tenemos la interfaz creada le asignamos ese tipo a nuestra respuesta
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+
+    //* En este array se iran guardando todos los pokemones en base a la promesa de la llamada a la API
+    const insertPromisesArray = []
 
     // Extraemos el nombre del pokemon y el id de la respuesta a la API
     data.results.forEach(async ({ name, url }) => {
@@ -30,9 +36,14 @@ export class SeedService {
       //? Una vez separada la url obtenemos el id en base a la posicion en que separo, en este caso la penultima 
       const no = +segments[segments.length - 2]
       //? Insertamos lo registros en la base de datos
-      const pokemon = await this.pokemonModel.create({ name, no })
+      //const pokemon = await this.pokemonModel.create({ name, no })
 
+      //* En lugar a que se espera que se cumpla y se inserten, primero guardamos la respuesta en el arreglo
+      insertPromisesArray.push(this.pokemonModel.create({ name, no }))
     })
+
+    //* Una vez que termino de recibir o almacenar las respuestas las incertamos
+    await Promise.all(insertPromisesArray)
 
     return 'Seed Excecuted';
   }
